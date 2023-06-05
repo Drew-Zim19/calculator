b52cdc42218f32ac5f6f2a2326cf7a2af320f291
Drew Zimmerman <drewjacobzimmerman@gmail.com>
2023-06-05 09:14:39 -0400

    Add basic screen display logic on 7 button, add clear button functionality, add backspace button functionality

--- calc.js
+++ calc.js
@@ -16,8 +16,10 @@ This is a calculator program. Below is the outline of how to solve
 let num1 = 1;
 let num2 = 3;
 let operator = ''
-let result = 0;
-
+let displayResult = [];
+let firstValue = 0;
+let isOperatorClicked = false;
+const screenDisplay = document.querySelector('.screen');
 
 //basic math operations functions
 function add(num1, num2){
@@ -43,3 +45,71 @@ function operate(operator, num1, num2){
         add(num1, num2);
     }
 }
+
+function inputNumber(number) {
+   if(isOperatorClicked === false){
+    displayResult.push(number);
+    document.querySelector('.screen').innerHTML = displayResult.join("");
+   }
+}
+
+document.getElementById('seven').addEventListener("click", () => {
+    inputNumber(7)
+});
+
+document.getElementById('eight').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('nine').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('four').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('five').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('six').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('one').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('two').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('three').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('zero').addEventListener("click", () => {
+
+
+});
+
+document.getElementById('cleared').addEventListener("click", () => {
+    document.querySelector('.screen').innerHTML = 0;
+    displayResult.length = 0;
+});
+
+document.getElementById('backspace').addEventListener("click", () => {
+    if(displayResult.length > 0){
+        displayResult.splice(displayResult.length - 1, 1);
+        document.querySelector('.screen').innerHTML = displayResult.join("");
+    }
+});
