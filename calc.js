923dd458b230f7a1b3d1b43595f7a2f758982002
Drew Zimmerman <drewjacobzimmerman@gmail.com>
2023-06-07 12:36:09 -0400

    Calculator functions as expected in console. Added logic to wire buttons

--- calc.js
+++ calc.js
@@ -16,6 +16,7 @@ This is a calculator program. Below is the outline of how to solve
 let num1 = 1;
 let num2 = 3;
 let operator = ''
+// array which holds values to be printed to screen
 let displayResult = [];
 let firstValue = 0;
 let isOperatorClicked = false;
@@ -40,17 +41,95 @@ function divide(num1, num2){
     return result;
 }
 
+
 function operate(operator, num1, num2){
-    if (operator = 'add'){
-        add(num1, num2);
+    let finalResult = 0;
+    if (operator === "add"){
+        finalResult = add(num1, num2);
     }
+    
+    if(operator === "minus"){
+        finalResult = subtract(num1, num2);
+    }
+    if(operator === "divide"){
+        finalResult = divide(num1, num2);
+    }
+    if(operator === "multiply"){
+        finalResult = multiply(num1, num2);
+    }
+    return finalResult;
 }
 
+//event listener on operators, if number has been input then display
+document.getElementById('add').addEventListener("click", () => {
+    if(displayResult.length > 0 && isOperatorClicked === false){
+        inputNumber('+');
+        isOperatorClicked = true;
+        operator = "add";
+    }
+});
+
+
+document.getElementById('divide').addEventListener("click", () => {
+    if(displayResult.length > 0 && isOperatorClicked === false){
+        inputNumber('/');
+        isOperatorClicked = true;
+        operator = "divide";
+    }
+
+});
+
+document.getElementById('minus').addEventListener("click", () => {
+    if(displayResult.length > 0 && isOperatorClicked === false){
+        inputNumber('-');
+        isOperatorClicked = true;
+        operator = "minus";
+    }
+
+});
+
+document.getElementById('multiply').addEventListener("click", () => {
+    if(displayResult.length > 0 && isOperatorClicked === false){
+        inputNumber('x');
+        isOperatorClicked = true;
+        operator = "multiply";
+    }
+
+});
+
 function inputNumber(number) {
-   if(isOperatorClicked === false){
     displayResult.push(number);
     document.querySelector('.screen').innerHTML = displayResult.join("");
-   }
+}
+
+//equals button, fires only when 2 numbers and an operator is supplied
+
+document.getElementById('equals').addEventListener("click", () => {
+    if(isOperatorClicked === true && displayResult.length > 2){
+        let copyArray = displayResult;
+        toNum(copyArray);
+        let finalResult = operate(operator, num1, num2);
+        console.log(finalResult);
+    }
+
+});
+
+//function to convert the array of numbers and operators to 2 numbers for calculations
+
+function toNum(array){
+    array = array.map(array => +array);
+    let operatorIndex = 0;
+
+    //logic to find index of NaN which is the operator
+
+    operatorIndex = array.findIndex(operatorIndex => Number.isNaN(operatorIndex));
+    
+    //remove the NaN then join the remaining numbers into 2 nums
+
+    array.splice(operatorIndex, 1);
+    num2Array = array.splice(0, operatorIndex);
+    num2 = Number(array.join(""));
+    num1 = Number(num2Array.join(""));
 }
 
 document.getElementById('seven').addEventListener("click", () => {
@@ -58,47 +137,47 @@ document.getElementById('seven').addEventListener("click", () => {
 });
 
 document.getElementById('eight').addEventListener("click", () => {
-
+    inputNumber(8)
 
 });
 
 document.getElementById('nine').addEventListener("click", () => {
-
+    inputNumber(9)
 
 });
 
 document.getElementById('four').addEventListener("click", () => {
-
+    inputNumber(4)
 
 });
 
 document.getElementById('five').addEventListener("click", () => {
-
+    inputNumber(5)
 
 });
 
 document.getElementById('six').addEventListener("click", () => {
-
+    inputNumber(6)
 
 });
 
 document.getElementById('one').addEventListener("click", () => {
-
+    inputNumber(1)
 
 });
 
 document.getElementById('two').addEventListener("click", () => {
-
+    inputNumber(2)
 
 });
 
 document.getElementById('three').addEventListener("click", () => {
-
+    inputNumber(3)
 
 });
 
 document.getElementById('zero').addEventListener("click", () => {
-
+    inputNumber(0)
 
 });
 
@@ -109,7 +188,14 @@ document.getElementById('cleared').addEventListener("click", () => {
 
 document.getElementById('backspace').addEventListener("click", () => {
     if(displayResult.length > 0){
-        displayResult.splice(displayResult.length - 1, 1);
+        let char = displayResult.splice(displayResult.length - 1, 1).join("");
+        console.log(char);
+        //check to make sure backspace wasnt an operator. if it was, change operator clicked to false
+
+        if(char === '-' || char === '+' || char === '/' || char === 'x'){
+            isOperatorClicked = false;
+        }
+
         document.querySelector('.screen').innerHTML = displayResult.join("");
     }
 });
